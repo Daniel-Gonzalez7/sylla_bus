0f835ef4b759526dadbb195ffb2ad7baeb440dca
Daniel <daniel_gonzalez07@yahoo.com>
2022-02-23 03:36:06 -0500

    fixed some form info, date selection

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -17,22 +17,22 @@ const MEETINGDAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
 const Form = () => {
   const [formData, setFormData] = useState({
-    courseTitle: "XYZ4000",
-    instructor: "My Instructor",
-    instructorEmail: "myinstructor@fsu.edu",
+    courseTitle: "",
+    instructor: "",
+    email: "",
     room: "",
-    school: "My University",
-    phoneNumber: "(813)-458-2674",
-    objectives: "Course Objectives",
-    academicPolicy: "Academic Policy",
+    school: "",
+    courseCode: "",
+    courseObjectives: "",
+    academicPolicy: "",
     textbook: "",
-    gradingNotes: "The Final Grade will be determined by ...",
+    gradingNotes: "",
     //figure out a way to do letter grade distribution
-    attendancePolicy: "Students are expected to show up for class",
-    accomodations: "Students with Disablilities ___",
+    attendancePolicy: "",
+    accomodations: "",
     meetingTimeStart: new Date("2020-01-01 12:00"),
     meetingTimeEnd: new Date("2020-01-01 12:00"),
-    meetingDates: [],
+    meetingDates: [true, false, true, false, true],
   });
 
   const FormButton = styled(
@@ -58,13 +58,25 @@ const Form = () => {
                 variant="filled"
                 label="Instructor Name"
                 placeholder="Daniel Gonzalez"
+                onChange={(e) => {
+                  setFormData({
+                    ...formData,
+                    instructor: e.target.value,
+                  });
+                }}
               />
             </Grid>
             <Grid item xs={12} sm={12} md={2}>
               <TextField
                 variant="filled"
                 label="Course Title"
-                placeholder="XXX4000"
+                placeholder="Intro to Geology"
+                onChange={(e) => {
+                  setFormData({
+                    ...formData,
+                    courseTitle: e.target.value,
+                  });
+                }}
               />
             </Grid>
             <Grid item xs={12} sm={12} md={2}>
@@ -72,6 +84,12 @@ const Form = () => {
                 variant="filled"
                 label="School Name"
                 placeholder="Florida State University"
+                onChange={(e) => {
+                  setFormData({
+                    ...formData,
+                    school: e.target.value,
+                  });
+                }}
               />
             </Grid>
             <Grid item xs={12} sm={12} md={2}>
@@ -79,6 +97,12 @@ const Form = () => {
                 variant="filled"
                 label="Instructor Email"
                 placeholder="dg18c@fsu.edu"
+                onChange={(e) => {
+                  setFormData({
+                    ...formData,
+                    email: e.target.value,
+                  });
+                }}
               />
             </Grid>
             <Grid item xs={12} sm={12} md={2}>
@@ -87,8 +111,11 @@ const Form = () => {
                   renderInput={(params) => <TextField {...params} />}
                   value={formData.meetingTimeStart}
                   label="Start Time"
-                  onChange={(newValue) => {
-                    setFormData({ ...formData, meetingTimeStart: newValue });
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      meetingTimeStart: e.target.value,
+                    });
                   }}
                 />
               </LocalizationProvider>
@@ -101,21 +128,40 @@ const Form = () => {
                   value={formData.meetingTimeEnd}
                   label="End Time"
                   onChange={(newValue) => {
-                    setFormData({ ...formData, meetingTimeEnd: newValue });
+                    setFormData({
+                      ...formData,
+                      meetingTimeEnd: newValue.toString(),
+                    });
                   }}
                 />
               </LocalizationProvider>
             </Grid>
 
             <Grid item xs={12} sm={12} md={2}>
-              <TextField variant="filled" label="Room" placeholder="EOA 1045" />
+              <TextField
+                variant="filled"
+                label="Room"
+                placeholder="EOA 1045"
+                onChange={(val) =>
+                  setFormData({
+                    ...formData,
+                    room: val.target.value,
+                  })
+                }
+              />
             </Grid>
 
             <Grid item xs={12} sm={12} md={2}>
               <TextField
                 variant="filled"
-                label="Phone Number"
-                placeholder="(XXX)-111-1111"
+                label="Course Code"
+                placeholder="XXX4000"
+                onChange={(val) =>
+                  setFormData({
+                    ...formData,
+                    courseCode: val.target.value,
+                  })
+                }
               />
             </Grid>
             <Grid item xs={12} sm={12} md={8}>
@@ -123,15 +169,24 @@ const Form = () => {
                 fullWidth={true}
                 variant="filled"
                 label="Textbook"
+                onChange={(val) =>
+                  setFormData({ ...formData, textbook: val.target.value })
+                }
                 placeholder="Understanding Earth by John Grotzinger & Thomas H. Jordan. First Edition."
               />
             </Grid>
             {MEETINGDAYS.map((day, i) => {
               return (
-                <Grid item xs={12} sm={12} md={2}>
+                <Grid key={`day ${day}`} item xs={12} sm={12} md={2}>
                   <FormControlLabel
-                    control={<Checkbox defaultChecked />}
+                    control={<Checkbox />}
                     label={day}
+                    checked={formData.meetingDates[i]}
+                    onChange={(e) => {
+                      let newDays = formData.meetingDates.slice();
+                      newDays[i] = e.target.checked;
+                      setFormData({ ...formData, meetingDates: newDays });
+                    }}
                   />
                 </Grid>
               );
@@ -144,6 +199,12 @@ const Form = () => {
                 label="Course Objectives"
                 placeholder="This course is designed to provide you with an overview of the 
 physical and chemical processes that have shaped the Earth in the past..."
+                onChange={(val) =>
+                  setFormData({
+                    ...formData,
+                    courseObjectives: val.target.value,
+                  })
+                }
               />
             </Grid>
 
@@ -155,6 +216,12 @@ physical and chemical processes that have shaped the Earth in the past..."
                 label="Grading Notes"
                 placeholder="Your final grade will be based on two midterm exams, a final exam and your
 final lab grade. Each exam is worth 25% and your lab score is worth 25%..."
+                onChange={(val) =>
+                  setFormData({
+                    ...formData,
+                    gradingNotes: val.target.value,
+                  })
+                }
               />
             </Grid>
             <Grid item xs={12} sm={12} md={12}>
@@ -165,6 +232,12 @@ final lab grade. Each exam is worth 25% and your lab score is worth 25%..."
                 label="Attendance Policy"
                 placeholder="Excused absences include documented illness, deaths in the family and other documented
 crises, call to active military duty or jury duty, religious holy days..."
+                onChange={(val) =>
+                  setFormData({
+                    ...formData,
+                    attendancePolicy: val.target.value,
+                  })
+                }
               />
             </Grid>
             <Grid item xs={12} sm={12} md={12}>
@@ -176,14 +249,44 @@ crises, call to active military duty or jury duty, religious holy days..."
                 placeholder="Students with disabilities needing academic accommodation should:
 (1) register with and provide documentation to the Student Disability Resource Center; and
 (2) bring a letter to the instructor indicating the need for accommodation and what type."
+                onChange={(val) =>
+                  setFormData({
+                    ...formData,
+                    accomodations: val.target.value,
+                  })
+                }
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+              <TextField
+                fullWidth={true}
+                multiline={true}
+                variant="filled"
+                label="Academic Honor Policy"
+                placeholder="The Florida State University Academic Honor Policy outlines the 
+University's expectations for the integrity of students' academic work, the procedures for 
+resolving alleged violations of those expectations, and the rights and responsibilities of students 
+and faculty members throughout the process.  Students are responsible for..."
+                onChange={(val) =>
+                  setFormData({
+                    ...formData,
+                    academicPolicy: val.target.value,
+                  })
+                }
               />
             </Grid>
           </Grid>
         </form>
-        <FormButton fullWidth>Generate Syllabus</FormButton>
+        <FormButton
+          fullWidth
+          onClick={() => alert("now we send over the data")}
+        >
+          Generate Syllabus
+        </FormButton>
       </Container>
     </>
   );
 };
+//academicPolicy
 
 export default Form;
